c6858955dc2171529f19732a518ec321777b0bc4
derrya <derryaditiya@local.local>
2018-05-19 14:25:15 +0700

    initial commit

---
+++ DemoStringFormat.java
@@ -0,0 +1,17 @@
+public class DemoStringFormat {
+
+    public static void main (String[] args){
+        // this should be in the file properties
+        String text = "Hello %s, today the weather id %s";
+
+
+        // this is parameter in java class
+        String nama = "derry";
+        String cuaca = "rainy";
+
+        String result = String.format(text, nama, cuaca);
+
+        System.out.println(result);
+    }
+
+}
